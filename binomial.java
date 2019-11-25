8a8fcd8c7b21a2bf3cca0b0d6cfcf30f9e2271cf
Stanley Quek <stanleyquekcp@gmail.com>
2019-11-26 01:08:20 +0800

    Added a few more functions

--- binomial.java
+++ binomial.java
@@ -2,15 +2,30 @@ import java.util.Scanner;
 
 class Binomial{
     public static void main(String[] args){
-        System.out.println(varExt("5x^7"));
+        System.out.println("4");
+        System.out.println(varExt("5"));
+        System.out.println(biCo(3,2));
     }
 
     public static void binomial(String exp, int power){
-        String[] binomials = exp.split("[+-]");
-        for(int i = 0; i < binomials.length ; i++){
-
+        String midOp = -;
+        if(exp.contains("+")){
+            midOp = "+";
         }
-        
+        String[] binomials = exp.split("[+-]");
+        String a = binomials[0];
+        String b = binomials[1];
+        int aC = coefficientExt(a);
+        int aV = varExt(a);
+        int aP = varExt(a);
+        int bC = coefficientExt(b);
+        int bV = varExt(b);
+        int bP = varExt(b);
+        for(int i = 0; i < power; i++){
+            if(aV == ' '){
+                System.out.printf("%d^%d");
+            }
+        }
     }
 
     public static int coefficientExt(String a){
@@ -26,8 +41,12 @@ class Binomial{
 
     public static char varExt(String a){
         int counter = 0;
-        while(a.charAt(counter) <= '9' && a.charAt(counter) >= '1'){
-            counter +=1;
+        try{
+            while(a.charAt(counter) <= '9' && a.charAt(counter) >= '1'){
+                counter +=1;
+            }
+        }catch(IndexOutOfBoundsException e){
+            return ' ';
         }
 
         return a.charAt(counter);
@@ -62,4 +81,8 @@ class Binomial{
         return b;
     }
 
+    public static int biCo(int n, int r) {
+        if (r == 0 || r == n) return 1;  
+        return biCo(n-1, r-1) + biCo(n-1, r); 
+    }
 }
